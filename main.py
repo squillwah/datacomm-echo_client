681e929e5dfd5388fd03710c7076a9a573a3f752
squillwah <Swagapotato@gmail.com>
2025-10-03 12:19:00 -0400

    command parser

---
+++ main.py
@@ -0,0 +1,64 @@
+import messages
+
+def command_parser(inpt: str) -> dict:
+    msg_data = {}
+
+    inpt.lstrip()           # Trim all left whitespace
+    words = inpt.split()    # Split input into list of all words (using space as delimiter)
+
+    # Go through each word and handle commands 
+    # Some commands are single worded, others may be followed by additional words, 
+    #  Ex: ;noecho (1 word) | ;spoof "IP" (2 words)
+    #
+    # Once the first word is found which is not preceeded by the command character (;)
+    # (and is not a part of a 2 word command), the rest of the word list from that
+    # word forward will be recombined and interpreted as the text of the message
+    word = 0
+    command = True
+    command_char = ';'
+    while (word < len(words) and command):
+        if not words[word].startswith(command_char):
+            command = False
+            continue
+
+        match words[word][1:]:
+            case "noecho":
+                msg_data["echo"] = False
+            case "spoof":                   # @todo actually implement spoofing
+                msg_data["spoof"] = True
+                word += 1
+                msg_data["spoof_ip"] = words[word]  # @todo check spoof ip formatting
+            case _:
+                print(f"Command Parsing Error: unkown setting {words[word][1:]}")
+
+        word += 1
+
+    msg_data["text"] = ' '.join(words[word:])
+
+    return msg_data
+
+def main() -> int:
+    print("Commands: ;noecho | ;spoof 'ip' | 'your message text here'")
+    print("Ex: ;noecho ;spoof 127.0.0.1 Hello World!\n")
+
+    inpt = input("Speak: ")
+    parsed = command_parser(inpt)
+
+    mess = messages.Message(parsed)
+
+    encoded = messages.encode_message(mess)
+    print(f"\n{encoded}\n")
+
+    decoded = messages.decode_message(encoded)
+    print(f"echo:{decoded.echo}")
+    print(f"text:{decoded.text}\n")
+
+    return 0
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
--- messages.py
+++ messages.py
@@ -1,11 +1,29 @@
 
 class Message:
-    format_keys = {"echo" : ("ECHOSTART", "ECHOEND"),
-                   "text" : ("TEXTSTART", "TEXTEND")}
+    # Keywords to wrap message data elements in during message transmission
+    FORMAT_KEYS = {"echo" : ("|ECHOSTART|", "|ECHOEND|"),
+                   "text" : ("|TEXTSTART|", "|TEXTEND|")}
 
-    def __init__(self):
-        self.echo = False
+    # Constructor, initializes message to default values + optional defined data 
+    def __init__(self, msg_data: dict = {}):
+        # Initial values
+        self.echo = True
         self.text = ""
+
+        # Parse optional value initialization dict
+        for key in msg_data:
+            bad_key = False
+            match key:
+                case "echo":
+                    if type(msg_data[key]) == bool: self.echo = msg_data[key]
+                    else: bad_key = True
+                case "text":
+                    if type(msg_data[key]) == str: self.text = msg_data[key]
+                    else: bad_key = True
+                case _:
+                    print(f"Message Initialization Error: unkown setting '{key}'")
+            if bad_key:
+                print(f"Message Initialization Error: bad setting type '{key}'")
 
 def _key_wrap(data, key: tuple((str, str))) -> str:
     # Wrap the data element around the given keys
@@ -19,20 +37,19 @@ def _key_unwrap(format_string: str, key: tuple((str, str))) -> str:
 
 def encode_message(msg: Message) -> bytes:
     format_string = ""              # String to hold 'serialized' message
-    format_string += _key_wrap(msg.echo, msg.format_keys["echo"])
-    format_string += _key_wrap(msg.text, msg.format_keys["text"])
+    format_string += _key_wrap(msg.echo, msg.FORMAT_KEYS["echo"])
+    format_string += _key_wrap(msg.text, msg.FORMAT_KEYS["text"])
     return format_string.encode()   # Return string encoded as bytes
-
 
 def decode_message(code: bytes) -> Message:
     # Create message, decode string message (formatted with protocol)
     msg = Message()
     format_string = code.decode()
     # Echo
-    echo = _key_unwrap(format_string, msg.format_keys["echo"])
-    msg.echo = bool(echo)
+    echo = _key_unwrap(format_string, msg.FORMAT_KEYS["echo"])
+    msg.echo = echo == "True"
     # Text
-    text = _key_unwrap(format_string, msg.format_keys["text"])
+    text = _key_unwrap(format_string, msg.FORMAT_KEYS["text"])
     msg.text = text
     # Return message
     return msg
