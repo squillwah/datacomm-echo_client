cab1745d66dc8dc3f8033b6849a3e1846e3b115c
squillwah <Swagapotato@gmail.com>
2025-10-16 16:44:38 -0400

    client cleanup and inbox methods

--- client.py
+++ client.py
@@ -2,39 +2,34 @@ from threading import Thread
 
 from messages import Message, encode_message, decode_message, stringify_message_fancy, stringify_message_raw
 from sock import socketConnection
-#from listener import Listener
 
-# Client
-
-# COMPOSED OF:
+# =============================================================================
+# Echo Client
 #
-# Socket connection
-# Single message to send
-# Queue of messages recieved
-
-# FUNCTIONALITY:
+# The echo client consists of:
+#  * a user written message, which can be stored, edited, and sent to the server
+#  * an inbox of messages recieved from the server, which can be viewed and emptied 
+#  * a socket connection to the server, wherein the ip and port can be specified
+#  * a threaded reciever, which silently processes bytes recieved on the connection
+#  * various settings which tweak the functionality of the client's operations
 #
-# Establish a connection
-# Destroy a connection
-#
-# Create and specify details of a message
-# 
-# Send message over connection
-#   Send ONE message at a time
-# Recieve message over connection
-#   Recieve MULTIPLE messages at a time, need SEPERATE THREAD
-#
+# The user interacts with the client through a command system defined in commands.py
+# =============================================================================
 
 class Client():
-    def __init__(self):
-        self._message = None     # Message written from client
-        self._inbox = []         # Messages recieved by client
-        self._connection = socketConnection()       # Socket manager
+    # --------------
+    # Initialization
+    # --------------
 
-        self._ls_thread = Thread(target=self._listener_process)
-        self._ls_buffer = b""
-        self._ls_running = False
-        self._ls_signal_recieved = False
+    def __init__(self):
+        self._message = None                    # Message written from client
+        self._inbox = []                        # Messages recieved by client
+        self._connection = socketConnection()   # Socket manager
+
+        self._ls_thread = Thread(target=self._listener_process) # Listener thread
+        #self._ls_buffer = b""                                   
+        self._ls_running = False            # Controls the listener recieve loop
+        self._ls_signal_recieved = False    # Signals True when a recieve occured
 
         self.flags = {"logging"     : False,    # Client logs its operations to screen
                       "force"       : False,    # Let messages in buffer be written over
@@ -45,12 +40,6 @@ class Client():
                       "burnonread"  : True}     # Delete the recieved message once read
 
         self.killme = False     # Signal client manager to stop processing this client
-
-
-    #def run()
-    #    self._running = True
-    #def stop()
-    #    self._running = False
 
     # =========================================================================
 
@@ -64,6 +53,7 @@ class Client():
         while(self._ls_running):
             #self._ls_buffer += self._connection.recv_msg()
             data = self._connection.recv_msg() # THIS COULD CAUSE ISSUE WITH INCOMPLETE MESSAGES MAYBE MAYBE MAYBE!
+            if self.flags["logging"]: print(" . message recieved")
 
             ## Search buffer for a valid message wrapped in the message key
             #buffer_string = str(ls_buffer)
@@ -89,11 +79,14 @@ class Client():
 
     # =========================================================================
 
-    # ---------------------------------
-    # Creating/Editing/Sending Messages
-    # ---------------------------------
+    # -------------------------
+    # Creating/Sending Messages
+    # -------------------------
 
-    def message_write(self, msg: Message):#, details: dict = {}):  # Details dict defines initial components of message
+    # Writes a message object to empty message buffer
+    #  Optionally overwrite occupied buffer with 'force' 
+    #  Optionally send message instantly with 'instantsend' 
+    def message_write(self, msg: Message):
         if self._message != None:
             print(" ! message in buffer")
             if self.flags["force"]:
@@ -106,65 +99,134 @@ class Client():
         if self.flags["logging"]: print(" . writing message to buffer")
         self._message = msg
         if self.flags["instantsend"]: self.message_send()
-        #if self.flags["instantread"]: self.display_message(self._recieved.pop()) # should this only be in listener?
 
-#        # instant read == wait to recieve echo before moving on
-#        recievedsize = len(self._recieved)
-#        self.message_send()
-#        while(len(self._recieved) == recievedsize) # wait before recieve
+    # Edits parts of the message in the buffer via dict w/ message component keys
+    def message_edit(self, changes: dict):
+        if self._message == None:
+            print(" ! no message in buffer to edit")
+            return
 
-        #NO!
-        #NO THREAD!
-        # start a timeout
-        # currentrecieved = len(self._recieved)
-        # sendtime
-        # self.message_send()
-        # while(len(self._recieved) ==
+        if self.flags["logging"]: print(" . updating message")
+        modify_message(self._message, changes)
 
-        # clear listener recieve flag
-        # wait while flag is not set
-        # then print the message
+    # Displays the written message in the buffer
+    def message_view(self):
+        if self._message == None:
+            print(" ! no message in buffer to view")
+            return
 
-        # should while loop here to wait for recieve listener with instant read?
+        print(f" {stringify_message_raw(self._message)}")
 
+    # Clears the message in the buffer
     def message_clear(self):
-        if self.flags["logging"]: print(" . clearing message in write buffer")
+        if self._message == None:
+            print(" ! no message in buffer to clear")
+            return
+
+        if self.flags["logging"]: print(" . clearing message in buffer")
         self._message = None
 
+    # Sends the message in the buffer through the connection socket
+    #  Optionally wait for and read the recieved echo with 'instantread'
+    #  Optionally clear the message buffer on send with 'burnonsend'
     def message_send(self):
-        # Clear the recieved signal
-        if self.flags["instantread"]: self._ls_signal_recieved = False
+        if self._connection.host == None:
+            print(" ! connection is closed, aborting send")
+            return
 
-        # Encode+send message, clear from buffer is burnonsend
+        # Clear the listener's recieved signal
+        self._ls_signal_recieved = False
+
+        # Encode+send message
         if self.flags["logging"]: print(" . encoding message in write buffer")
         msgbytes = encode_message(self._message)
         if self.flags["logging"]: print(" . sending encoded message through socket")
         self._connection.send_msg(msgbytes)
+
+        # Delete message if set to burnonsend
         if self.flags["burnonsend"]: self.message_clear()
 
         # If in instantread mode, wait for the recieve signal then display
         if self.flags["instantread"]:
-            if self.flags["logging"]: print(" . waiting for recieve (instantread set)")
-            while not self._ls_signal_recieved: pass
-            self.display_message(self._inbox.pop())
+            if not self._ls_signal_recieved:
+                if self.flags["logging"]: print(" . waiting for recieve")
+                while not self._ls_signal_recieved: pass
+            self.inbox_read_top()
 
-
-
-
-        #if self.flags["logging"]: print(" . waiting for recieve...")
-        #self._recieved.append(decode_message(self._connection.recv_msg()))
+    #def message_recieve() #? should we even bother with the thread, or should we silently halt until its recieved?
 
     def display_message(self, msg: Message):
-        #print(f'text: "{msg.text}"')
-        #print(f"echo: {msg.echo}\n")
-
         print(f" {stringify_message_fancy(msg)}")
         print(f" {stringify_message_raw(msg)}")
 
-    #def message_view()
-    #def message_edit()
-    #def inbox_view()
-    #def inbox_empty()
+    # =========================================================================
+
+    # ----------------------
+    # Reading/Emptying Inbox
+    # ----------------------
+
+    #read to view most recent message
+    #read all to view all message in inbox
+
+    # Displays the most recent message added to inbox
+    #  Optionally delete the message with 'burnonread'
+    #  Optionally display raw message data with 'rawread'
+    def inbox_read_top(self):
+        if len(self._inbox) == 0:
+            print(" ! inbox empty")
+            return
+
+        reader = stringify_message_fancy
+        if self.flags["rawread"]: reader = stringify_message_raw
+
+        messindex = len(self._inbox)-1
+        print(f"\n  {reader(self._inbox[messindex])}\n")
+
+        if self.flags["burnonread"]: self.inbox_delete(self, messindex))
+
+    # Displays all messages in inbox
+    #  Optionally empty inbox with 'burnonread'
+    #  Optionally display messages raw with 'rawread'
+    def inbox_read_all(self):
+        if len(self._inbox) == 0:
+            print(" ! inbox empty")
+            return
+
+        reader = stringify_message_fancy
+        if self.flags["rawread"]: reader = stringify_message_raw
+
+        print()
+        for messindex in range(len(self._inbox), 0):
+            print(f"  {messindex}. {reader(self._inbox(messindex)}")
+        print()
+
+        if self.flags["burnonread"]: self.inbox_empty()
+
+    # Delete message at index in inbox
+    def inbox_delete(self, index: int):
+        if index < 0 or >= len(self._inbox):
+            print(f" ! inbox message {index+1} doesn't exist")
+            return
+
+        if self.flags["logging"]: print(f" . deleting message {index+1}")
+        del self._inbox[index]
+
+    # Delete all messages in inbox
+    def inbox_empty(self):
+        if len(self._index) == 0:
+            print(f" ! inbox already empty")
+            return
+
+        if self.flags["logging"]: print(f" . emptying message {index+1}")
+        for i in range(0, len(self._inbox)): self.inbox_delete(i)
+        # alternatively: self._inbox = []
+
+
+    # =========================================================================
+
+    # ----------------------------------
+    # Setting/Opening/Closing Connection
+    # ----------------------------------
 
     def connection_set_ip(self, ip: str):
         if self.flags["logging"]: print(f"CLOG: Setting connection host to {ip}")
@@ -189,6 +251,13 @@ class Client():
         if self.flags["logging"]: print(" . closing connection")
         self._connection.close()
 
+    # =========================================================================
+
+    # ---------------------
+    # Meta Client Functions
+    # ---------------------
+
+    # Returns a dict describing the state of each client component
     def get_state(self) -> dict:
         state = {"connection":((self._connection.sock is not None)*"Active" + (self._connection.sock is None)*"Disconnected"),
                  "connectionhost":self._connection.host,
@@ -200,17 +269,11 @@ class Client():
             state["flags"][flag] = self.flags[flag]*"on" + (not self.flags[flag])*"off"
         return state
 
-    def shutdown(self):
-        self.connection_close()
-        # stop the listener
-        self.killme = True
-
-
-
-
-            #    self.message_wait_for_recieve() threaded listerner DO!
-    # message_display() 
-
+    # Gracefully shutdown the client
+    #def shutdown(self):
+    #    self.connection_close()
+    #    # stop the listener
+    #    self.killme = True
 
 
 
