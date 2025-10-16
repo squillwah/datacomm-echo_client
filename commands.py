077813ac0f5c9e2dd883d4a6b32b6cfc252cac0e
squillwah <Swagapotato@gmail.com>
2025-10-16 19:39:07 -0400

    mega commands rejigger

--- commands.py
+++ commands.py
@@ -2,7 +2,7 @@ from enum import Enum
 from dataclasses import dataclass
 
 from client import Client
-from messages import Message, parse_message_command
+from messages import Message, parse_message_data_string
 
 # =============================================================================
 # Here lies all commands possible with our echo client:
@@ -11,55 +11,32 @@ from messages import Message, parse_message_command
 # Command logic is defined in single functions which operate on Client objects.
 # =============================================================================
 
-# ----------------------
-# Command Types and Data
-# ----------------------
-
-# Types of commands in the client
-class CommandCode(Enum):
-    Null = None
-    Help = 0            # Help text
-    Status = 1          # Client state
-    Write = 2           # Write message
-    Set = 3             # Set a client flag
-    Quit = 4
-
-
-#    view # view message in buffer
-#    clear # clear message in buffer
-#    edit # edit message in buffer
-#    send # send message in buffer (manual)
-#
-# keepwrite # everything written is a message sent : text : text
-#
-#    read # read messages recieved
-#    clear # clear messages recieved
-
-    #quit
-
-# Command data container 
-@dataclass
-class Command():
-    opcode: CommandCode
-    operands: list[str]
-
-# =============================================================================
-
 # ------------------
 # Command procedures
 # ------------------
 
 # HELP
 # Prints instructions on how to use the client
-def helpme(cmds: list[str]):
+def cmd_help(operands: list[str]):
+    cmds = operands
     if len(cmds) == 0:
         print(" [Commands]: ")
         print("  * help")
         print("  * status")
         print("  * write")
-        print("  * set\n")
+        print("  * set")
+        print("  * quit\n")
         print("  Run this command with commands you want help with (or all for all commands)")
         print("  ex: 'help status' / 'help write status' / 'help all'\n")
+
+        print(" [Client Flags]: ")
+        print("  * logging      enable logging of client operations")
+        print("  * force        disable message overwrite protection")
+        print("  * rawread      disable message modifier formatting")
+        print("  * instantsend  instantly send a message on write")
+        print("  * instantread  instantly read a message on echo recieve")
+        print("  * burnonsend   clear your message after sending")
+        print("  * burnonread   delete your recieved echos once read\n")
 
         print(" [Writing Messages]:")
         print("  Message modifiers begin with ';'.")
@@ -94,12 +71,16 @@ def helpme(cmds: list[str]):
                 print(" [set]")
                 print("  sets a client flag on or off")
                 print("  ex: 'set instantsend on' / 'set instantread off'")
+            case "quit":
+                print(" [quit]")
+                print("  shuts down connection and quits the client")
+                print("  ex: 'quit'")
             case _:
                 print(f" ! can't help you with '{cmd}'")
 
 # STATUS
 # Displays the state of the client's components
-def status(client: Client):
+def cmd_status(client: Client, operands = None):
     client_state = client.get_state()
     #print(" Connection: " + (client.connection.sock is not None)*"Active" + (client.connection.sock is None)*"Closed")
     #print(" Message Buffer: " + (client.message is not None)*"Full" + (client.message is None)*"Empty")
@@ -125,23 +106,84 @@ def status(client: Client):
 
 # WRITE
 # Creates a Message from string & writes it to the Client's write buffer
-def write(client: Client, msg_definition_str: str):
-    mess = Message(parse_message_command(msg_definition_str)) # @? should parsing the message data be done HERE or in MESSAGES?
+def cmd_write(client: Client, operands: list[str]):
+    if len(operands) > 0: msg_definition_str = operands[0]
+    else: msg_definition_str = ""   # allow for blank messages
+
+    mess = Message(parse_message_data_string(msg_definition_str)) # @? should parsing the message data be done HERE or in MESSAGES?
     client.message_write(mess)
 
 # SET
 # Toggle parts of the client on/off
-def setflag(client: Client, flag: str, onoff: bool):
+def cmd_set(client: Client, operands: list[str]):
+    if len(operands) != 2:
+        print(" ! bad set command, must be 3 words (set flag on/off)")
+        return
+
+    flag = operands[0]
+    onoff = operands[1]
     if flag in client.flags:
-        client.flags[flag] = onoff
-        print(f" {flag}: {client.flags[flag]}")
+        if onoff in ["on", "off"]:
+            client.flags[flag] = onoff == "on"
+            print(f" {flag}: {client.flags[flag]}")
+        else: print(f" ! flags can only be set on or of, not '{operands[1]}'")
     else: print(f" ! unknown client setting '{flag}'")
 
 # QUIT
 # Gracefully shutdown client connection, set the client kill signal
-def quit(client: Client):
+def cmd_quit(client: Client, operands = None):
     client.connection_close()
     client.killme = True
+
+# VIEW
+# View the message in buffer
+def cmd_view(client: Client, operands = None):
+    client.message_view()
+
+# EDIT
+# Edit the message in buffer
+def cmd_edit(client: Client, operands: list[str]):
+    if len(operands) > 0: msg_definition_str = operands[0]
+    else: msg_definition_str = ""   # allow for empty edits
+
+    client.message_edit(parse_message_data_string(msg_definition_str))
+
+# =============================================================================
+
+# ----------------------
+# Command Types and Data
+# ----------------------
+
+# Types of commands in the client + associated procedures
+class CommandCode(Enum):
+    Null    = None
+    Help    = cmd_help #0            # Help text
+    Status  = cmd_status #1          # Client state
+    Write   = cmd_write  #2           # Write message
+    Set     = cmd_set #3             # Set a client flag
+    Quit    = cmd_quit #4
+
+    View = cmd_view #5            # View message written in buffer
+    Edit = cmd_edit #6            # Edit message written in buffer
+
+
+#    clear # clear message in buffer
+#    edit # edit message in buffer
+#    send # send message in buffer (manual)
+#
+# keepwrite # everything written is a message sent : text : text
+#
+#    read # read messages recieved
+#    clear # clear messages recieved
+
+    #quit
+
+# Command data container 
+@dataclass
+class Command():
+    opcode: CommandCode     # Command type and related procedure
+    operands: list[str]     # List of string word operands
+    signature: int          # Arguments needed by command procedure
 
 # =============================================================================
 
@@ -152,67 +194,140 @@ def quit(client: Client):
 # COMMAND_GET
 # Returns a Command dataclass interpreted from the given string
 def command_get(inpt: str) -> Command:
-    opcode = CommandCode.Null
-    operands = []
+    #opcode = CommandCode.Null   # Null command
+    #operands = []               # No operands
+    #signature = (False, False)  # Empty signature
 
+    # Process words of command
     cmdwords = inpt.lower().split()
+
+    # If operands given, add them
+    if len(cmdwords) > 1: operands = cmdwords[1:]
+    else: operands = []
+
+    # Set opcode and signature
+    opcode = CommandCode.Null   # Default null command
+    signature = 0b00            # Empty signature
     match cmdwords[0]:
-        # HELP
-        # 0 -> n operands
         case "help":
             opcode = CommandCode.Help
-            if len(cmdwords) > 1: operands = cmdwords[1:]   # Add help operands (specific commands to explain)
-        # STATUS
-        # 0 operands
+            signature = 0b01
         case "status":
             opcode = CommandCode.Status
-        # WRITE
-        # 1 operand
+            signature = 0b10
         case "write":
             opcode = CommandCode.Write
-            if len(cmdwords) > 1: operands = [inpt[6:]]     # Add rest of command (message modifiers and text) 
-            else: operands = [""]                           # If only "write" was written, specify it as blank
-        # SET
-        # 2 operands
+            signature = 0b11
+            # Special operand case: use single raw input string preserve text spacing
+            if len(operands) > 0: operands = [inpt[6:]]
         case "set":
             opcode = CommandCode.Set
-            operands = ["", ""]
-            if len(cmdwords) != 3: print(f" ! bad set command, must be 3 words (set flag on/off)")
-            else: operands = [cmdwords[1], cmdwords[2]]
-        # QUIT
-        # 0 operands
+            signature = 0b11
         case "quit":
             opcode = CommandCode.Quit
-        # INVALID
-        # 0 operands/CommandCode.Null
+            signature = 0b10
+        case "view":
+            opcode = CommandCode.View
+            signature = 0b10
+        case "edit":
+            opcode = CommandCode.Edit
+            signature = 0b11
+            # Special operand case: use single raw input string preserve text spacing
+            if len(operands) > 0: operands = [inpt[5:]]
         case _:
             print(f" ! unknown command '{inpt}'")
 
-    return Command(opcode, operands)
+    return Command(opcode, operands, signature)
+
+#if cmdwords[0] == "help"
+#
+#    match cmdwords[0]:
+#        # HELP
+#        # 0 -> n operands
+#        # (0, 1) signature
+#        case "help":
+#            opcode = CommandCode.Help
+#            if len(cmdwords) > 1: operands = cmdwords[1:]   # Add help operands (specific commands to explain)
+#        # STATUS
+#        # 0 operands
+#        case "status":
+#            opcode = CommandCode.Status
+#        # WRITE
+#        # 1 operand
+#        case "write":
+#            opcode = CommandCode.Write
+#            if len(cmdwords) > 1: operands = [inpt[6:]]     # Add rest of command (message modifiers and text) 
+#            else: operands = [""]                           # If only "write" was written, specify it as blank
+#        # SET
+#        # 2 operands
+#        case "set":
+#            opcode = CommandCode.Set
+#            operands = ["", ""]
+#            if len(cmdwords) != 3: print(f" ! bad set command, must be 3 words (set flag on/off)")
+#            else: operands = [cmdwords[1], cmdwords[2]]
+#        # QUIT
+#        # 0 operands
+#        case "quit":
+#            opcode = CommandCode.Quit
+#        # VIEW
+#        # 0 operands
+#        case "view":
+#            opcode = CommandCode.View
+#        # EDIT
+#        # 1 operand
+#        case "edit":
+#            opcode = CommandCode.Edit
+#            if len(cmdwords) > 1: operands = [inpt[5:]]     # Same deal as 'write' add rest of command (message modifiers and text) 
+#            else: operands = [""]                           # If only "edit" was written, specify it as blank
+#        # INVALID
+#        # 0 operands/CommandCode.Null
+#        case _:
+#            print(f" ! unknown command '{inpt}'")
+#
+#    return Command(opcode, operands)
 
 # COMMAND_RUN
 # Executes the given command's procedure on a Client object
 def command_run(client: Client, cmd: Command) -> bool:
     success = True
-    match cmd.opcode:
-        case CommandCode.Null:
-            print(" hint: use the 'help' command")
-        case CommandCode.Help:
-            helpme(cmd.operands)
-        case CommandCode.Status:
-            status(client)
-        case CommandCode.Write:
-            print(cmd.operands)
-            write(client, cmd.operands[0])
-        case CommandCode.Set:
-            if cmd.operands[1] == "on": setflag(client, cmd.operands[0], True)
-            elif cmd.operands[1] == "off": setflag(client, cmd.operands[0], False)
-            else: print(f" ! flags can only be set on or of, not '{cmd.operands[1]}'")
-        case CommandCode.Quit:
-            quit(client)
-        case _:
-            print(f" ! unknown command type '{cmd}'\n")
-            success = False
+    print(cmd)
+    print(cmd.opcode)
+
+    if cmd.opcode != CommandCode.Null:
+        match cmd.signature:
+            case 0b01: cmd.opcode(cmd.operands)
+            case 0b10: cmd.opcode(client)
+            case 0b11: cmd.opcode(client, cmd.operands)
+            case _:
+                success = False
+                print(" ! bad command signature, how did that happen?")
+    else:
+        print(" hint: use the 'help' command")
+        #print(f" ! unknown command type '{cmd}'\n")
+        success = False
+
+    #match cmd.opcode:
+    #    case CommandCode.Null:
+    #        print(" hint: use the 'help' command")
+    #    case CommandCode.Help:
+    #        helpme(cmd.operands)
+    #    case CommandCode.Status:
+    #        status(client)
+    #    case CommandCode.Write:
+    #        print(cmd.operands)
+    #        write(client, cmd.operands[0])
+    #    case CommandCode.Set:
+    #        if cmd.operands[1] == "on": setflag(client, cmd.operands[0], True)
+    #        elif cmd.operands[1] == "off": setflag(client, cmd.operands[0], False)
+    #        else: print(f" ! flags can only be set on or of, not '{cmd.operands[1]}'")
+    #    case CommandCode.Quit:
+    #        quit(client)
+    #    case CommandCode.View:
+    #        view(client)
+    #    case CommandCode.
+    #    case _:
+    #        print(f" ! unknown command type '{cmd}'\n")
+    #        success = False
     print() # newline between commands
     return success
 
