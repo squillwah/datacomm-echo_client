ce5176f651907c982885278e0c6838ba20303091
squillwah <Swagapotato@gmail.com>
2025-10-17 01:22:13 -0400

    working launch script

--- mainexample.py
+++ mainexample.py
@@ -1,5 +1,5 @@
 from client import Client
-from commands import run_command, get_command
+from commands import command_get, command_run
 
 print("running echo-client set-up script")
 
@@ -12,22 +12,14 @@ client = Client()
 print("setting client connection host to 158.83.11.22")
 command_run(client, command_get("host 158.83.11.22"))
 
-print("enter desired port: ")
-while(not command_run(client, command_get("port " + input()))): print("try that again: ")
+# @todo fix this input check not working
+while(not command_run(client, command_get("port " + input("enter port: ")))): pass
 
 print("establishing connection")
 command_run(client, command_get("connect"))
 
-print("client set-up done, running input loop\n")
+print("client set-up done, running input loop")
+print("try 'help' for a list of commands\n")
 while not client.killme: command_run(client, command_get(input("> ")))
-
-# client.killme signals that whatever is managing the client should end processing with it
-# client.killme should really only be modified by the graceful client.shutdown() but ig in
-# event of emergency it can be set manually and 'force killed'
-
-
-
-
-
 
 
