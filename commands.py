9bf2eaa6cea81375098e78d50465a7cf6942a8e5
squillwah <Swagapotato@gmail.com>
2025-10-17 00:27:59 -0400

    better help/organization, completed message+inbox commands

--- commands.py
+++ commands.py
@@ -12,8 +12,13 @@ from messages import Message, parse_message_data_string
 # =============================================================================
 
 # ------------------
-# Command procedures
+# Command Procedures
 # ------------------
+
+# -----------------------------
+#     HELP/STATUS/SET/QUIT
+# miscellaneous client commands
+# -----------------------------
 
 # HELP
 # Prints instructions on how to use the client
@@ -21,13 +26,16 @@ def cmd_help(operands: list[str]):
     cmds = operands
     if len(cmds) == 0:
         print(" [Commands]: ")
-        print("  * help")
-        print("  * status")
-        print("  * write")
-        print("  * set")
-        print("  * quit\n")
-        print("  Run this command with commands you want help with (or all for all commands)")
-        print("  ex: 'help status' / 'help write status' / 'help all'\n")
+        print("  [Client]:")
+        print("   * help * status * set * quit")
+        print("  [Messages]:")
+        print("   * write * view * edit * clear * send * simple")
+        print("  [Inbox]:")
+        print("   * read * delete * empty")
+        print("  [Connection]:")
+        print("   * host * port * connect * disconnect\n")
+        print("  Run this command with commands or command groups you want help with (or all for all commands)")
+        print("  ex: 'help status' / 'help write status delete' / 'help messages' / 'help all'\n")
 
         print(" [Client Flags]: ")
         print("  * logging      enable logging of client operations")
@@ -50,9 +58,21 @@ def cmd_help(operands: list[str]):
         print("  * ;caps     capitalize all letters")
         print("  * ;reverse  reverse the message")
         print("  * ;text     explicitly begin your message's text")
-
-    elif "all" in cmds:
-        cmds = ["help", "status", "write", "set"]
+    else:
+        if "all" in cmds: cmds = ["client", "messages", "inbox", "connection"]
+        if "client" in cmds:
+            cmds += ["help", "status", "set", "quit"]
+            cmds.remove("client")
+        if "messages" in cmds:
+            cmds += ["write", "view", "edit", "clear", "send", "simple"]
+            cmds.remove("messages")
+        if "inbox" in cmds:
+            cmds += ["read", "delete", "empty"]
+            cmds.remove("inbox")
+        if "connection" in cmds:
+            cmds += ["host", "port", "connect", "disconnect"]
+            cmds.remove("connection")
+        cmds = list(dict.fromkeys(cmds)) # remove duplicates
     for cmd in cmds:
         match cmd:
             case "help":
@@ -80,7 +100,7 @@ def cmd_help(operands: list[str]):
 
 # STATUS
 # Displays the state of the client's components
-def cmd_status(client: Client, operands = None):
+def cmd_status(client: Client):
     client_state = client.get_state()
 
     print(f" Connection: {client_state["connection"]}")
@@ -92,15 +112,6 @@ def cmd_status(client: Client, operands = None):
 
     for flag in client_state["flags"]:
         print(f"  {flag}: {client_state["flags"][flag]}")
-
-# WRITE
-# Creates a Message from string & writes it to the Client's write buffer
-def cmd_write(client: Client, operands: list[str]):
-    if len(operands) > 0: msg_definition_str = operands[0]
-    else: msg_definition_str = ""   # allow for blank messages
-
-    mess = Message(parse_message_data_string(msg_definition_str)) # @? should parsing the message data be done HERE or in MESSAGES?
-    client.message_write(mess)
 
 # SET
 # Toggle parts of the client on/off
@@ -120,13 +131,26 @@ def cmd_set(client: Client, operands: list[str]):
 
 # QUIT
 # Gracefully shutdown client connection, set the client kill signal
-def cmd_quit(client: Client, operands = None):
+def cmd_quit(client: Client):
     client.connection_close()
     client.killme = True
 
+# ---------------------------------
+# WRITE/VIEW/EDIT/CLEAR/SEND/SIMPLE
+#         message commands
+# ---------------------------------
+
+# WRITE
+# Creates a Message from string & writes it to the client's write buffer
+def cmd_write(client: Client, operands: list[str]):
+    if len(operands) > 0: msg_definition_str = operands[0]
+    else: msg_definition_str = ""   # allow for blank messages
+    mess = Message(parse_message_data_string(msg_definition_str)) # @? should parsing the message data be done HERE or in MESSAGES?
+    client.message_write(mess)
+
 # VIEW
 # View the message in buffer
-def cmd_view(client: Client, operands = None):
+def cmd_view(client: Client):
     client.message_view()
 
 # EDIT
@@ -134,8 +158,73 @@ def cmd_view(client: Client, operands = None):
 def cmd_edit(client: Client, operands: list[str]):
     if len(operands) > 0: msg_definition_str = operands[0]
     else: msg_definition_str = ""   # allow for empty edits
-
     client.message_edit(parse_message_data_string(msg_definition_str))
+
+# CLEAR
+# Clear message from client write buffer
+def cmd_clear(client: Client):
+    client.message_clear()
+
+# SEND
+# Send message in buffer over connection
+def cmd_send(client: Client):
+    client.message_send()
+
+# SIMPLE
+# Basic send and echo mode, loops writing and sending a new message each input
+def cmd_simple(client: Client):
+    print(" entering simple echo mode")
+    print(" type 'complex' before your message text/modifiers to quit\n")
+
+    # Change needed flags for simple echo mode
+    flagstates = {"force":client.flags["force"],
+                  "instantsend":client.flags["instantsend"],
+                  "instantread":client.flags["instantread"],
+                  "burnonsend":client.flags["burnonsend"],
+                  "burnonread":client.flags["burnonread"]}
+
+    client.flags["force"] = True
+    client.flags["instantsend"] = True
+    client.flags["instantread"] = True
+    client.flags["burnonsend"] = True
+    client.flags["burnonread"] = True
+
+    # Write input loop
+    inpt = input(": ")
+    while inpt != "complex":
+        command_run(client, command_get("write " + inpt))
+        inpt = input(": ")
+
+    print( "exiting simple echo mode")
+
+    # Restore flag states
+    for flag in flagstates:
+        client.flags[flag] = flagstates[flag]
+
+# -----------------
+# READ/DELETE/EMPTY
+#  inbox commands
+# -----------------
+
+def cmd_read(client: Client, operands: list[str]):
+    if len(operands) > 1:
+        print(" ! bad read command, must be 1 or 2 words (read / read all / read n)")
+        return
+    if len(operands) == 0: client.inbox_read_top()                    # No operands, read top
+    elif operands[0] == "all": client.inbox_read_all()                # "All", list entire inbox
+    elif operands[0].isdigit(): client.inbox_read(int(operands[0])-1) # Int, read msg at index
+    else: print(f" ! can't read '{operands[0]}', only (read / read all / read n) accepted")
+
+def cmd_delete(client: Client, operands: list[str]):
+    if len(operands) != 1:
+        print(" ! bad delete command, must be 2 words (delete n)")
+        return
+    if operands[0].isdigit(): client.inbox_delete(int(operands[0])-1)
+    else: print(f" ! can't delete '{operands[0]}', must be inbox message number")
+
+def cmd_empty(client: Client):
+    client.inbox_empty()
+
 
 # =============================================================================
 
@@ -157,16 +246,19 @@ class CommandCode(Enum):
     Edit    = cmd_edit #6            # Edit message written in buffer
     Clear   = cmd_clear     # clear message in buffer
     Send    = cmd_send      # send message in buffer
-    Simple              # keep write mode
+    Simple  = cmd_simple              # keep write mode
     # Inbox
-    Read # read, read 1, read all
-    Delete  # delete 1 delete 2
-    Empty   # empty whole inbox, (or should it be delete all?)
-    # Connection
-    Host        # set socket host
-    Port        # set socket port
-    Connect     # connect to socket
-    Disconnect  # disconnect from socket
+    Read    = cmd_read # read top message
+    Delete  = cmd_delete # delete at inbox index 
+    Empty   = cmd_empty # delete all
+    #Read # read, read 1, read all
+    #Delete  # delete 1 delete 2
+    #Empty   # empty whole inbox, (or should it be delete all?)
+    ## Connection
+    #Host        # set socket host
+    #Port        # set socket port
+    #Connect     # connect to socket
+    #Disconnect  # disconnect from socket
 
 # Command data container 
 @dataclass
@@ -201,17 +293,17 @@ def command_get(inpt: str) -> Command:
         case "status":
             opcode = CommandCode.Status
             signature = 0b10
-        case "write":
-            opcode = CommandCode.Write
-            signature = 0b11
-            # Special operand case: use single raw input string to preserve text spacing
-            if len(operands) > 0: operands = [inpt[6:]]
         case "set":
             opcode = CommandCode.Set
             signature = 0b11
         case "quit":
             opcode = CommandCode.Quit
             signature = 0b10
+        case "write":
+            opcode = CommandCode.Write
+            signature = 0b11
+            # Special operand case: use single raw input string to preserve text spacing
+            if len(operands) > 0: operands = [inpt[6:]]
         case "view":
             opcode = CommandCode.View
             signature = 0b10
@@ -220,6 +312,24 @@ def command_get(inpt: str) -> Command:
             signature = 0b11
             # Special operand case: use single raw input string to preserve text spacing
             if len(operands) > 0: operands = [inpt[5:]]
+        case "clear":
+            opcode = CommandCode.Clear
+            signature = 0b10
+        case "send":
+            opcode = CommandCode.Send
+            signature = 0b10
+        case "simple":
+            opcode = CommandCode.Simple
+            signature = 0b10
+        case "read":
+            opcode = CommandCode.Read
+            signature = 0b11
+        case "delete":
+            opcode = CommandCode.Delete
+            signature = 0b11
+        case "empty":
+            opcode = CommandCode.Empty
+            signature = 0b10
         case _:
             print(f" ! unknown command '{inpt}'")
 
@@ -229,9 +339,6 @@ def command_get(inpt: str) -> Command:
 # Executes the given command's procedure on a Client object
 def command_run(client: Client, cmd: Command) -> bool:
     success = True
-    print(cmd)
-    print(cmd.opcode)
-
     if cmd.opcode != CommandCode.Null:
         match cmd.signature:
             case 0b01: cmd.opcode(cmd.operands)
@@ -242,9 +349,7 @@ def command_run(client: Client, cmd: Command) -> bool:
                 print(" ! bad command signature, how did that happen?")
     else:
         print(" hint: use the 'help' command")
-        #print(f" ! unknown command type '{cmd}'\n")
         success = False
-
     print() # newline between commands
     return success
 
